c86e9ac5a768ba0510c882c3f51926072acaf5d2
sunilom563 <63774321+sunilom563@users.noreply.github.com>
2022-03-01 17:42:23 +0100

    add news and tech component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopheadingComponent } from './topheading/topheading.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NewsapiservicesService } from './service/newsapiservices.service';
+import { TechnewsComponent } from './technews/technews.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheadingComponent,
+    TechnewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NewsapiservicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
