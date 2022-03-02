9f9bdfb7cba4ebabb2d0bfdf1ba8229ea8f257ea
sunilom563 <sunilbdeshanur@gmail.com>
2022-03-02 19:52:31 +0100

    adeed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TopheadingComponent } from './topheading/topheading.component';
-import { TechnewsComponent } from './technews/technews.component'
+import { TechnewsComponent } from './technews/technews.component';
+import {BuisnessComponent} from './buisness/buisness.component'
 
 const routes: Routes = [
   {path: '', component: TopheadingComponent},     //home
-  {path: 'tech', component: TechnewsComponent}   //technews
+  {path: 'tech', component: TechnewsComponent},  //technews
+  {path: 'busine', component: BuisnessComponent},
 ];
 
 @NgModule({
